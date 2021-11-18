d3e0dcd035a64a6f53d88a702bff26c695ac8edd
molkers <molki2488@gmail.com>
2021-11-18 20:48:52 +0000

    movie search functionality added with apiKey

--- my.js
+++ my.js
@@ -1,1 +1,40 @@
 //js code 
+
+//get Api key from http://www.omdbapi.com/ and paste it here
+const apiKey = '';
+
+const resultsSection = document.getElementById('results');
+
+const searchFilm = (e) => {
+    const userInput = document.getElementById('searchBar').value;
+    resultsSection.innerHTML = ' ';
+    callApi(userInput);
+} 
+
+const callApi = (title) => {
+    fetch(`http://www.omdbapi.com/?s=${title}&apikey=${apiKey}`)
+    .then( res => res.json() )
+    .then( 
+            data => displayMovies(data)  
+            
+        );
+}
+
+const displayMovies = ( res ) => {
+    
+    
+    try {
+        res.Search.forEach(movie => {
+            resultsSection.innerHTML += `<pre> ${movie.Title} </pre> `
+            console.log(movie.Title)
+        });
+      }
+      catch(err) {
+        //error
+        console.log('error');
+      }
+
+    //resultsSection.innerHTML = `<pre> ${ JSON.stringify(res) } </pre> `
+}
+
+
