6d1a6310f54f56fa30cf121b42e757c5e1b5b573
molkers <molki2488@gmail.com>
2021-11-19 18:15:22 +0000

    comments added

--- my.js
+++ my.js
@@ -3,14 +3,17 @@
 //get Api key from http://www.omdbapi.com/ and paste it here
 const apiKey = '';
 
+//declare results element as variable resultsSection
 const resultsSection = document.getElementById('results');
 
+//get users input from searchBar
 const searchFilm = (e) => {
     const userInput = document.getElementById('searchBar').value;
     resultsSection.innerHTML = ' ';
     callApi(userInput);
 } 
 
+//call endpoint to get movies data 
 const callApi = (title) => {
     fetch(`http://www.omdbapi.com/?s=${title}&apikey=${apiKey}`)
     .then( res => res.json() )
@@ -20,6 +23,7 @@ const callApi = (title) => {
         );
 }
 
+//display results in resultsSection
 const displayMovies = ( res ) => {
     
     
